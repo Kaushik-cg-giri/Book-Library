46be1166d7d52f96d5ed87e62a2ed2cee32feaac
Owner <Owner@DESKTOP-E6LVFQN>
2025-10-06 10:54:11 +0530

    return type changed to optional

--- src/main/java/com/book_management/repository/BookRepository.java
+++ src/main/java/com/book_management/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.book_management.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.book_management.entity.Book;
 @Repository
 public interface BookRepository  extends JpaRepository<Book, Long>{
 
-	Book findByAuthor(String author);
+		Optional<Book> findByAuthor(String author);
 }
